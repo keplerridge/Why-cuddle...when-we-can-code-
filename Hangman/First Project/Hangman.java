4ec42e876a255622058a30a2a31749314263953a
cddewitt <cddewitt@bsu.edu>
2016-06-27 23:26:34 -0500

    added simple scanner

--- Hangman/First Project/Hangman.java
+++ Hangman/First Project/Hangman.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 
 public class Hangman {
 
+	
+	public static void main(String[] args){
+		System.out.println("Please enter your letter guess");
+		Scanner scanner = new Scanner(System.in);
+		String letter = scanner.next();
+		System.out.println(letter);
+		scanner.close();
+		
+	}
 }
